f434ab0badb2b9c7ea801d0a91b7671b5f6dacfd
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-02-01 11:26:02 -0300

    Média 1

---
+++ C++/1000-1099/1005.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double A, B, MEDIA;
+
+    cin >> A;
+    cin >> B;
+    MEDIA = (A * 3.5 + B * 7.5)/11;
+
+    cout << fixed;
+    cout.precision(5);
+    cout << "MEDIA = " << MEDIA << endl;
+
+    return 0;
+}
