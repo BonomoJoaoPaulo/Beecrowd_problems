8a1e179b5f269656cb98c47a55832bccac56856a
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-02-01 10:58:57 -0300

    Extremamente Básico

---
+++ C++/1000-1099/1001.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n1, n2, res;
+    cin >> n1;
+    cin >> n2;
+
+    res = n1 + n2;
+
+    cout << "X = " << res << "\n";
+    return 0;
+}
