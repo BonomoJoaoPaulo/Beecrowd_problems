52621cae38c1e330f5bac83592157dd348107bf0
BonomoJoaoPaulo <jpabonomo@gmail.com>
2023-02-01 12:55:31 -0300

    Distância

---
+++ C++/1000-1099/1016.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(){
+    int vX = 60, vY = 90, distance, time;
+
+    cin >> distance;
+    
+    time = distance * 2;
+
+    cout << time << " minutos" << endl;
+
+    return 0;
+}
